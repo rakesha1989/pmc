34d793e70210b2e07a1b42a6cedc4c595e881cbb
rakesha1989 <rakesh.qubik@gmail.com>
2015-11-09 17:37:40 +0530

    added role in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,17 @@ has_many :clients
 has_many :projects
 has_many :vendors
 has_many :permissions
-has_many :roles, through: :permissions 
+has_many :roles, through: :permissions
+
+def role?(role)
+		self.roles.pluck(:name).include?(role)
+end
+
+def setup_user_role
+		if user.roles.empty?
+			user.roles << Role.third
+	end
+end 
 
 
 
