2850c423463023eae5e6c197ed5de8e5fca033ec
rakesha1989 <rakesh.qubik@gmail.com>
2015-11-09 14:40:59 +0530

    added name to model user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107094545) do
+ActiveRecord::Schema.define(version: 20151109080039) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20151107094545) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
